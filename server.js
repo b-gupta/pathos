7b2d34bcf70af6f49ec059d5caa2131d04afeb1c
Bharat Gupta <bgupta91@gmail.com>
2018-03-14 01:53:33 -0400

    remove unnecessary check

--- server.js
+++ server.js
@@ -8,24 +8,24 @@ app.use(bodyParser.urlencoded({ extended: true }))
 var map = {};
 
 app.post('/messages', function(req, res) {
-	console.log(req.body.message);
-	var hash = sha256(req.body.message);
-	map[hash] = req.body.message;
-	res.send([{
-		"digest": hash
-	}]);
+    console.log(req.body.message);
+    var hash = sha256(req.body.message);
+    map[hash] = req.body.message;
+    res.send([{
+        "digest": hash
+    }]);
 });
 
 app.get('/messages/:hash', function(req, res) {
-	console.log(req.params.hash);
-	if (!map.hasOwnProperty(req.params.hash)) {
-		res.status(404).send([{ "err_msg" : "Not found!"}]);
-	}
-	else {
-		res.send([{
-			"message": map[req.params.hash]
-		}]);
-	}
+    console.log(req.params.hash);
+    if (!map.hasOwnProperty(req.params.hash)) {
+        res.status(404).send([{ "err_msg" : "Not found!"}]);
+    }
+    else {
+        res.send([{
+            "message": map[req.params.hash]
+        }]);
+    }
 });
 
 app.listen(3001);
--- x01.py
+++ x01.py
@@ -1,17 +1,17 @@
 import sys
 
 def perm_x(s, i):
-	if len(s) == i:
-		print s
-		return
-	
-	if s[i] == 'x':
-		tokens = s.partition('x')
-		perm_x(tokens[0] + '0' + tokens[2], i+1)
-		perm_x(tokens[0] + '1' + tokens[2], i+1)
-	else:
-		perm_x(s, i+1)
+    if len(s) == i:
+        print s
+        return
+    
+    if s[i] == 'x':
+        tokens = s.partition('x')
+        perm_x(tokens[0] + '0' + tokens[2], i+1)
+        perm_x(tokens[0] + '1' + tokens[2], i+1)
+    else:
+        perm_x(s, i+1)
 
 if __name__ == '__main__':
-	args = sys.argv[1:]
-	perm_x(args[0].lower(), 0)
+    args = sys.argv[1:]
+    perm_x(args[0].lower(), 0)
